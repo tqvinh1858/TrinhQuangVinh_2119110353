b29dcd74f20921b352a3dae9628e606dd9b4dcb2
Vinh999 <247.tqvinh@gmail.com>
2022-01-13 17:39:29 +0700

    Kết nối CSDL bằng Store Proc

--- Cau1/DAL/EmployeeDAL.cs
+++ Cau1/DAL/EmployeeDAL.cs
@@ -1,6 +1,7 @@
 ﻿using Cau1.DAO;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -38,44 +39,150 @@ namespace Cau1.DAL
         }
         public void DeleteEmployee(EmployeeDAO emp)
         {
+            //SqlConnection conn = CreateConnection();
+            //conn.Open();
+            //SqlCommand cmd = new SqlCommand("delete from Employee_2119110353 where IdEmployee = @IdEmployee", conn);
+            //cmd.Parameters.Add(new SqlParameter("@IdEmployee", emp.IdEmployee));
+            //cmd.ExecuteNonQuery();
+            //conn.Close();
+
             SqlConnection conn = CreateConnection();
             conn.Open();
-            SqlCommand cmd = new SqlCommand("delete from Employee_2119110353 where IdEmployee = @IdEmployee", conn);
-            cmd.Parameters.Add(new SqlParameter("@IdEmployee", emp.IdEmployee));
-            cmd.ExecuteNonQuery();
-            conn.Close();
+
+            try
+            {
+                //khỏi tạo instance của class SqlCommand
+                SqlCommand cmd = new SqlCommand();
+                //sử dụng thuộc tính CommandText để chỉ định tên Proc
+                cmd.CommandText = "spDeleteEmployee";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+
+                //khai báo các thông tin của tham số truyền vào
+                cmd.Parameters.Add("@IdEmployee", SqlDbType.NVarChar).Value = emp.IdEmployee;
+                //mở chuỗi kết nối
+                conn.Open();
+                //sử dụng ExecuteNonQuery để thực thi
+                cmd.ExecuteNonQuery();
+                //đóng chuỗi kết nối.
+                conn.Close();
+
+                Console.WriteLine("Xoa hoc sinh thanh cong !!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Co loi xay ra !!!" + e);
+            }
+            // dóng chuỗi kết nối
+            finally
+            {
+                conn.Close();
+            }
         }
         public void NewEmployee(EmployeeDAO emp)
         {
+            //SqlConnection conn = CreateConnection();
+            //conn.Open();
+            //SqlCommand cmd = new SqlCommand(
+            //    "insert into Employee_2119110353(IdEmployee,NameEmp,IdDepartment,PlaceBirth,DateBirth,Gender) values(@IdEmployee,@NameEmp,@IdDepartment,@PlaceBirth,@DateBirth,@Gender)", conn);
+            //cmd.Parameters.Add(new SqlParameter("@NameEmp", emp.NameEmp));
+            //cmd.Parameters.Add(new SqlParameter("@IdDepartment", emp.Department.IdDepartment));
+            //cmd.Parameters.Add(new SqlParameter("@PlaceBirth", emp.PlaceBirth));
+            //cmd.Parameters.Add(new SqlParameter("@DateBirth", emp.DateBirth));
+            //cmd.Parameters.Add(new SqlParameter("@Gender", emp.Gender));
+            //cmd.Parameters.Add(new SqlParameter("@IdEmployee", emp.IdEmployee));            
+            //cmd.ExecuteNonQuery();
+            //conn.Close();
+
             SqlConnection conn = CreateConnection();
             conn.Open();
-            SqlCommand cmd = new SqlCommand(
-                "insert into Employee_2119110353(IdEmployee,NameEmp,IdDepartment,PlaceBirth,DateBirth,Gender) values(@IdEmployee,@NameEmp,@IdDepartment,@PlaceBirth,@DateBirth,@Gender)", conn);
-            cmd.Parameters.Add(new SqlParameter("@NameEmp", emp.NameEmp));
-            cmd.Parameters.Add(new SqlParameter("@IdDepartment", emp.Department.IdDepartment));
-            cmd.Parameters.Add(new SqlParameter("@PlaceBirth", emp.PlaceBirth));
-            cmd.Parameters.Add(new SqlParameter("@DateBirth", emp.DateBirth));
-            cmd.Parameters.Add(new SqlParameter("@Gender", emp.Gender));
-            cmd.Parameters.Add(new SqlParameter("@IdEmployee", emp.IdEmployee));            
-            cmd.ExecuteNonQuery();
-            conn.Close();
+            try
+            {
+                //khỏi tạo instance của class SqlCommand
+                SqlCommand cmd = new SqlCommand();
+                //sử dụng thuộc tính CommandText để chỉ định tên Proc
+                cmd.CommandText = "spInsertEmployee";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+
+                //khai báo các thông tin của tham số truyền vào
+                cmd.Parameters.Add("@IdEmployee", SqlDbType.NVarChar).Value = emp.IdEmployee;
+                cmd.Parameters.Add("@NameEmp", SqlDbType.NVarChar).Value = emp.NameEmp;
+                cmd.Parameters.Add("@IdDepartment", SqlDbType.NVarChar).Value = emp.Department;
+                cmd.Parameters.Add("@PlaceBirth", SqlDbType.NVarChar).Value = emp.PlaceBirth;
+                cmd.Parameters.Add("@DateBirth", SqlDbType.Date).Value = emp.DateBirth;
+                cmd.Parameters.Add("@Gender", SqlDbType.Int).Value = emp.Gender;
+                //mở chuỗi kết nối
+                conn.Open();
+                //sử dụng ExecuteNonQuery để thực thi
+                cmd.ExecuteNonQuery();
+                //đóng chuỗi kết nối.
+                conn.Close();
+                Console.WriteLine("Them thanh cong !!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Co loi xay ra !!!" + e);
+            }
+            // dóng chuỗi kết nối
+            finally
+            {
+                conn.Close();
+            }
         }
         public void EditEmployee(EmployeeDAO emp)
         {
+            //SqlConnection conn = CreateConnection();
+            //conn.Open();
+            //SqlCommand cmd = new SqlCommand(
+            //    "update Employee_2119110353 set NameEmp = @NameEmp, IdEmployee = @IdEmployee," +
+            //    "IdDepartment=@IdDepartment, PlaceBirth=@PlaceBirth," +
+            //    "DateBirth=@DateBirth, Gender=@Gender where @IdEmployee = IdEmployee", conn);
+            //cmd.Parameters.Add(new SqlParameter("@IdEmployee", emp.IdEmployee));
+            //cmd.Parameters.Add(new SqlParameter("@NameEmp", emp.NameEmp));
+            //cmd.Parameters.Add(new SqlParameter("@IdDepartment", emp.Department.IdDepartment));
+            //cmd.Parameters.Add(new SqlParameter("@PlaceBirth", emp.PlaceBirth));
+            //cmd.Parameters.Add(new SqlParameter("@DateBirth", emp.DateBirth));
+            //cmd.Parameters.Add(new SqlParameter("@Gender", emp.Gender));
+            //cmd.ExecuteNonQuery();
+            //conn.Close();
+
             SqlConnection conn = CreateConnection();
             conn.Open();
-            SqlCommand cmd = new SqlCommand(
-                "update Employee_2119110353 set NameEmp = @NameEmp, IdEmployee = @IdEmployee," +
-                "IdDepartment=@IdDepartment, PlaceBirth=@PlaceBirth," +
-                "DateBirth=@DateBirth, Gender=@Gender where @IdEmployee = IdEmployee", conn);
-            cmd.Parameters.Add(new SqlParameter("@IdEmployee", emp.IdEmployee));
-            cmd.Parameters.Add(new SqlParameter("@NameEmp", emp.NameEmp));
-            cmd.Parameters.Add(new SqlParameter("@IdDepartment", emp.Department.IdDepartment));
-            cmd.Parameters.Add(new SqlParameter("@PlaceBirth", emp.PlaceBirth));
-            cmd.Parameters.Add(new SqlParameter("@DateBirth", emp.DateBirth));
-            cmd.Parameters.Add(new SqlParameter("@Gender", emp.Gender));
-            cmd.ExecuteNonQuery();
-            conn.Close();
+            try
+            {
+                //khỏi tạo instance của class SqlCommand
+                SqlCommand cmd = new SqlCommand();
+                //sử dụng thuộc tính CommandText để chỉ định tên Proc
+                cmd.CommandText = " spEditEmployee";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+
+                //khai báo các thông tin của tham số truyền vào
+                cmd.Parameters.Add("@IdEmployee", SqlDbType.NVarChar).Value = emp.IdEmployee;
+                cmd.Parameters.Add("@NameEmp", SqlDbType.NVarChar).Value = emp.NameEmp;
+                cmd.Parameters.Add("@IdDepartment", SqlDbType.NVarChar).Value = emp.Department;
+                cmd.Parameters.Add("@PlaceBirth", SqlDbType.NVarChar).Value = emp.PlaceBirth;
+                cmd.Parameters.Add("@DateBirth", SqlDbType.Date).Value = emp.DateBirth;
+                cmd.Parameters.Add("@Gender", SqlDbType.Int).Value = emp.Gender;
+                //mở chuỗi kết nối
+                conn.Open();
+                //sử dụng ExecuteNonQuery để thực thi
+                cmd.ExecuteNonQuery();
+                //đóng chuỗi kết nối.
+                conn.Close();
+
+                Console.WriteLine("Sua thanh cong !!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Co loi xay ra !!!" + e);
+            }
+            // dóng chuỗi kết nối
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
